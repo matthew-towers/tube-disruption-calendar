f48ea039b410d060e42a50791b694ffb059a0fdd
Matthew Towers <matthew.towers@gmail.com>
2024-11-15 08:27:01 +0000

    notify of errors

--- auto.py
+++ auto.py
@@ -1,15 +1,24 @@
 import git
 import disruption_calendar
 import os
+import sys
 # Set up a Repo object for the repository storing the tube disruption calendar. Pull the latest files. Update the calendars, add and commit the new calendar files, then push to the remote repo.
 # gitpython docs: https://gitpython.readthedocs.io/en/stable/index.html
 
-cwd = os.getcwd()
-repo = git.Repo(cwd)
-o = repo.remotes.origin
+try:
+    cwd = os.getcwd()
+    repo = git.Repo(cwd)
+    o = repo.remotes.origin
+except:
+    os.system("notify-send 'Error setting up repo'")
+    sys.exit(0)
 
 print("pulling...")
-o.pull()
+try:
+    o.pull()
+except:
+    os.system("notify-send 'Error pulling'")
+    sys.exit(0)
 
 disruption_calendar.make_all_calendars()
 
@@ -17,5 +26,9 @@ repo.git.add(all=True)
 repo.index.commit("update calendars")
 
 print("pushing...")
-o.push()
-os.system("notify-send 'calendars updated'")
+
+try:
+    o.push()
+    os.system("notify-send 'calendars updated'")
+except:
+    os.system("notify-send 'Error pushing'")
