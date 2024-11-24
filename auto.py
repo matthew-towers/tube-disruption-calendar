7a10f607443e674220912b0775093f71113a1395
Matt Towers <matthew.towers@gmail.com>
2024-11-24 10:00:03 +0000

    update calendars

--- auto.py
+++ auto.py
@@ -3,12 +3,16 @@ import git
 import disruption_calendar
 import os
 import sys
+import pathlib
 # Set up a Repo object for the repository storing the tube disruption calendar. Pull the latest files. Update the calendars, add and commit the new calendar files, then push to the remote repo.
 # gitpython docs: https://gitpython.readthedocs.io/en/stable/index.html
+print(__file__)
 
 try:
-    cwd = os.getcwd()
-    repo = git.Repo(cwd)
+    # cwd = os.getcwd()
+    this_file = __file__
+    this_dir = pathlib.Path(__file__).parent.resolve()
+    repo = git.Repo(this_dir)
     o = repo.remotes.origin
 except:
     os.system("notify-send 'Error setting up repo'")
