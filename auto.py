48f19a7142ee0aab92a4e4eca0e6d0f4e4e7a224
Matt Towers <matthew.towers@gmail.com>
2024-12-08 10:56:41 +0000

    don't update unless the calendars are >12h old

--- auto.py
+++ auto.py
@@ -4,9 +4,11 @@ import disruption_calendar
 import os
 import sys
 import pathlib
+import datetime as dt
 # Set up a Repo object for the repository storing the tube disruption calendar.
-# Pull the latest files. Update the calendars, add and commit the new calendar
-# files, then push to the remote repo.
+# Pull the latest files. If the latest commit was more than 12 hours ago,
+# update the calendars, add and commit the new calendar files, then push to the
+# remote repo.
 #
 # gitpython docs: https://gitpython.readthedocs.io/en/stable/index.html
 
@@ -18,17 +20,24 @@ try:
     repo = git.Repo(this_dir)
     o = repo.remotes.origin
 except:
-    os.system("notify-send 'Error setting up repo'")
+    os.system("notify-send 'Tube disruption calendar' 'Error setting up repo'")
     sys.exit(0)
 
 print("pulling...")
 try:
     o.pull()
 except:
-    os.system("notify-send 'Error pulling'")
+    os.system("notify-send 'Tube disruption calendar' 'Error pulling'")
     sys.exit(0)
 
-disruption_calendar.make_all_calendars()
+latest_commit_date = repo.head.commit.authored_datetime
+tz = dt.datetime.now(dt.timezone.utc).astimezone().tzinfo
+current_date = dt.datetime.now(tz)
+if current_date - latest_commit_date > dt.timedelta(hours=12):
+    disruption_calendar.make_all_calendars()
+else:
+    os.system("notify-send 'Tube disruption calendar' 'Too recent to update'")
+    sys.exit(0)
 
 repo.git.add(all=True)
 repo.index.commit("update calendars")
