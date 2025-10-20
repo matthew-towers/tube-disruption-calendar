7b2bc5e3d437954ecc158188192eada1a7d38d72
Matt Towers <matthew.towers@gmail.com>
2025-10-20 19:46:42 +0100

    add logging

--- auto.py
+++ auto.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
-import git
+import datetime as dt
 import disruption_calendar
+import git
+import logging
 import os
 import sys
 import pathlib
-import datetime as dt
+
+file_log = logging.FileHandler(filename="tdc.log")
+print_log = logging.StreamHandler(stream=sys.stdout)
+handlers = [file_log, print_log]
+logging.basicConfig(
+        level=logging.INFO,
+        handlers=handlers,
+        format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'
+        )
+logger = logging.getLogger(__name__)
 
 # Set up a Repo object for the repository storing the tube disruption calendar.
 # Pull the latest files. If the latest commit was more than 12 hours ago, or if
@@ -21,14 +32,16 @@ try:
     o = repo.remotes.origin
 except:
     os.system("notify-send -a 'Tube disruption calendar' 'Error setting up repo'")
+    logger.exception("Error setting up repo.")
     sys.exit(1)
 
-print("pulling...")
+logger.info("pulling")
 
 try:
     o.pull()
 except:
     os.system("notify-send -a 'Tube disruption calendar' 'Error pulling'")
+    logger.exception("Error pulling.")
     repo.close()
     sys.exit(1)
 
@@ -41,26 +54,27 @@ args = sys.argv
 force = len(args) == 2 and args[1] == "force"
 
 if force or time_since_last_commit > dt.timedelta(hours=12):
-    print(f"last commit was this long ago: {time_since_last_commit}.")
+    logger.info(f"Last commit was this long ago: {time_since_last_commit}.")
     disruption_calendar.make_all_calendars()
 else:
     os.system("notify-send -a 'Tube disruption calendar' 'Not updating, too recent'")
-    print("Not updating - latest commit was too recent.")
+    logger.info("Not updating - latest commit was too recent.")
     repo.close()
     sys.exit(1)
 
 repo.git.add(all=True)
 repo.index.commit("update calendars")
 
-print("pushing...")
+logger.info("pushing...")
 
 try:
     o.push()
 except:
     os.system("notify-send -a 'Tube disruption calendar' 'Error pushing'")
+    logger.exception("Error pushing.")
     repo.close()
     sys.exit(1)
 
 os.system("notify-send -a 'Tube disruption calendar' 'calendars updated'")
-print("Push successful.")
+logger.info("Push successful.")
 repo.close()
